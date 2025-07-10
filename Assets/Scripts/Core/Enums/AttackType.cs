e7ca9390969cd41e39424fac313859b3f9b0b7f6
Mahmud Esad Yazar <113360249+ESADev@users.noreply.github.com>
2025-07-10 10:57:33 +0300

    Add core enums for faction, resources, attacks and interactions

---
+++ Assets/Scripts/Core/Enums/AttackType.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+/// <summary>
+/// Specifies the type of attack performed by a unit or ability.
+/// </summary>
+public enum AttackType
+{
+    /// <summary>
+    /// A close-quarters melee attack.
+    /// </summary>
+    Melee,
+
+    /// <summary>
+    /// A ranged attack that can hit targets from a distance.
+    /// </summary>
+    Ranged
+}
